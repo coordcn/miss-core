ffcc6045d7d8f085127535f90a30890a9562fc9d
qianye <qianye@droi.com>
2017-05-18 14:17:46 +0800

    new

---
+++ src/mime.lua
@@ -0,0 +1,19 @@
+-- Copyright © 2017 coord.cn. All rights reserved.
+-- @author      QianYe(coordcn@163.com)
+-- @license     MIT license
+
+local _M = {
+        PLAIN                   = "text/plain",
+        HTML                    = "text/html",
+        JSON                    = "application/json",
+        JAVASCRIPT              = "application/javascript",
+        XML                     = "application/xml",
+        TEXT_XML                = "text/xml",
+        FORM                    = "application/x-www-form-urlencoded",
+        MULTIPART               = "multipart/form-data",
+        OCTET                   = "application/octet-stream",
+        PROTOBUF                = "application/protobuf",
+        MSGPACK                 = "application/msgpack",
+}
+
+return _M
