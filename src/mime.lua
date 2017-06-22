4ecc51de8853b6f239bde7863b30ea46927be10c
coordcn <coordcn@163.com>
2017-06-22 18:09:26 +0800

    add some mime type

--- src/mime.lua
+++ src/mime.lua
@@ -3,17 +3,25 @@
 -- @license     MIT license
 
 local _M = {
+        TEXT                    = "text/plain",
         PLAIN                   = "text/plain",
         HTML                    = "text/html",
         JSON                    = "application/json",
         JAVASCRIPT              = "application/javascript",
+        JSONP                   = "text/javascript",
         XML                     = "application/xml",
         TEXT_XML                = "text/xml",
         FORM                    = "application/x-www-form-urlencoded",
         MULTIPART               = "multipart/form-data",
         OCTET                   = "application/octet-stream",
+        BIN                     = "application/octet-stream",
+        EXE                     = "application/octet-stream",
+        CLASS                   = "application/octet-stream",
         PROTOBUF                = "application/protobuf",
         MSGPACK                 = "application/msgpack",
+        PDF                     = "application/pdf",
+        DOC                     = "application/msword",
+        DOT                     = "application/msword",
 }
 
 return _M
