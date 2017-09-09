aadc112533c91e0a1f0c8012e131b00a0c28f686
coordcn <coordcn@163.com>
2017-09-09 15:21:32 +0800

    add utils.dump(tab) and utils.deepEqual(a, b)

--- src/utils.lua
+++ src/utils.lua
@@ -10,9 +10,11 @@ local _M = {}
 -- @param   str     {string}
 -- @param   wrapper {string}
 -- @return  str     {string}
-function _M.wrap(str, wrapper)
+local function wrap(str, wrapper)
     return wrapper .. str .. wrapper
 end
+
+_M.wrap = wrap
 
 -- @brief   remove regexp from both ends of a string
 -- @param   str     {string}
@@ -84,7 +86,7 @@ end
 -- @param   tab     {object}
 -- @param   except  {object[boolean]}
 -- @return  keys    {array[string]}
-function _M.keys(tab, except)
+local function keys(tab, except)
     local keys = {}
     if except then
         for key, val in pairs(tab) do
@@ -100,6 +102,8 @@ function _M.keys(tab, except)
 
     return keys
 end
+
+_M.keys = keys
 
 -- @brief   merge src table to dest table
 -- @param   dest    {object}
@@ -144,9 +148,132 @@ function _M.parseContentType(str)
     return ctype, charset
 end
 
+local function isArray(tab)
+    if type(tab) ~= "table" then
+        return false
+    end
+
+    local i = 1
+    for k, v in pairs(tab) do
+        if k ~= i then
+            return false
+        end 
+        i = i + 1
+    end
+
+    return true
+end
+
+_M.isArray = isArray
+
+local function indent(level)
+    return string.rep("    ", level)
+end
+
+local function quote(str)
+    return '"' .. string.gsub(str, '"', '\\"') .. '"'
+end
+
+local function wrapKey(key)
+    if type(key) == "string" then
+        return "[" .. quote(key) .. "]"
+    else
+        return "[" .. tostring(key) .. "]"
+    end
+end
+
+local function dump(input, level)
+    local t = type(input)
+    if t == "string" then
+        return quote(input)
+    elseif t == "table" then
+        local i     = 1
+        local lines = {}
+        if isArray(input) then
+            for k, v in ipairs(input) do
+                local s = dump(v, level + 1)
+                lines[i] = s
+                i = i + 1
+            end
+        else
+            local max = 0
+            for k, v in pairs(input) do
+                local key = wrapKey(k)
+                local l = #key + 1
+                if l > max then
+                    max = l
+                end
+            end
+
+            local len = math.ceil(max / 4) * 4
+            for k, v in pairs(input) do
+                local key = wrapKey(k) 
+                local s = key .. string.rep(" ", len - #key) .. "= "
+                s = s .. dump(v, level + 1)
+                lines[i] = s
+                i = i + 1
+            end
+        end
+
+        local content = table.concat(lines, ",\n" .. indent(level))
+        return "{\n" .. indent(level) .. content .. "\n" .. indent(level -1) .. "}"
+    else
+        return tostring(input)
+    end
+end
+
 -- @brief   dump table
---
-function _M.dump(tab)
+-- @param   input   {any}
+-- @return  str     {string}
+-- @refer   https://github.com/luvit/luv/blob/master/lib/utils.lua
+function _M.dump(input)
+    return dump(input, 1)
+end
+
+local function deepEqual(a, b, path)
+    if a == b then
+        return true
+    end
+
+    local atype = type(a)
+    local btype = type(b)
+    if atype ~= btype then
+        local prefix = path or "."
+        return false, prefix .. " not equal (" .. tostring(a) .. ", " .. tostring(b) .. ")."
+    end
+
+    if atype ~= "table" then
+        local prefix = path or "."
+        return false, prefix .. " not equal (" .. tostring(a) .. ", " .. tostring(b) .. ")."
+    end
+
+    for key in pairs(a) do
+        print(key)
+        local newPath   = path and (path .. "." .. key) or ("." .. key)
+        local same, msg = deepEqual(a[key], b[key], newPath)
+        if not same then
+            return same, msg
+        end
+    end
+
+    for key in pairs(b) do
+        if a[key] == nil then
+            local newPath   = path and (path .. "." .. key) or ("." .. key)
+            return false, newPath .. " not equal (" .. tostring(a[key]) .. ", " .. tostring(b[key]) .. ")."
+        end
+    end
+
+    return true
+end
+
+-- @brief   deep equal
+-- @param   a       {any}
+-- @param   b       {any}
+-- @return  same    {boolean}
+-- @return  msg     {string}
+-- @refer   https://github.com/luvit/luvit/blob/master/tests/libs/deep-equal.lua
+function _M.deepEqual(a, b)
+    return deepEqual(a, b)
 end
 
 return _M
